df9e59a5b15a1e118c673670732590ce48bc9965
macbre <maciej.brencz@gmail.com>
2012-07-07 19:18:39 +0200

    phantomas: parse script arguments

--- phantomas.js
+++ phantomas.js
@@ -6,15 +6,17 @@
  *    --url=<page to check>
  *    --verbose
  *
- *
  * @version 0.1
  */
 
+// @see https://github.com/jgonera/phantomjs-nodify#how-to-use
 var nodify = 'lib/phantomjs-nodify/nodify.js';
 phantom.injectJs(nodify);
 
 nodify.run(function() {
-	// ...
-	console.log('Kaboom!');
+	// parse script arguments
+	var args = require('./lib/args').parse(phantom.args);
+
+	console.log(args);
 	phantom.exit();
 });
