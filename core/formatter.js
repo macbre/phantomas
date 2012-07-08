43508b249435ebd390e90abe2ea4ef42f70b36ce
macbre <maciej.brencz@gmail.com>
2012-07-08 12:58:57 +0200

    Analyze HTTP request + results formatter

---
+++ core/formatter.js
@@ -0,0 +1,41 @@
+/**
+ * Results formatter
+ */
+var formatter = function(results, format) {
+	function render() {
+		switch(format) {
+			case 'json':
+				return formatJson();
+
+			case 'plain':
+			default:
+				return formatPlain();
+		}
+	};
+
+	function formatJson() {
+		return JSON.stringify(results);
+	};
+
+	function formatPlain() {
+		var res = '',
+		    obj = results.metrics,
+		    key;
+
+		for (key in obj) {
+			res += '* ' + key + ': ' + obj[key]+ '\n';
+		};
+
+		results.notices.forEach(function(msg) {
+			res += '> ' + msg + "\n";
+		});
+
+		return res;
+	};
+
+	// public interface
+	this.render = render;
+};
+
+exports.formatter = formatter;
+
