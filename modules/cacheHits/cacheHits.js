16069ca46ab8bec70544d490628ef220421b106b
macbre <maciej.brencz@gmail.com>
2012-07-16 22:40:35 +0200

    Add cacheHits metrics module

---
+++ modules/cacheHits/cacheHits.js
@@ -0,0 +1,28 @@
+/**
+ * Analyzes X-Cache headers from caching servers like Squid or Varnish
+ */
+exports.version = '0.1';
+
+exports.module = function(phantomas) {
+	phantomas.setMetric('cacheHits');
+	phantomas.setMetric('cacheMisses');
+
+	var re = /miss|hit/i;
+	
+	// examples:
+	// X-Cache:HIT, HIT
+	// X-Cache:arsenic miss (0)
+	phantomas.on('recv', function(entry,res) {
+		var header = entry.headers['X-Cache'] || '',
+			isHit;
+
+		if (re.test(header)) {
+			isHit = header.toLowerCase().indexOf('hit') > -1;
+			phantomas.incrMetric(isHit ? 'cacheHits' : 'cacheMisses');
+
+			if (!isHit) {
+				phantomas.addNotice('Cache miss on ' + entry.url + ' (X-Cache: ' + header + ')');
+			}
+		}
+	});
+};
--- phantomas.js
+++ phantomas.js
@@ -24,6 +24,7 @@ nodify.run(function() {
 
 	// add 3rd party modules
 	instance.addModule('assetsTypes');
+	instance.addModule('cacheHits');
 	instance.addModule('domComplexity');
 	//instance.addModule('domQueries'); // FIXME: jQuery mockup generates random issues
 	instance.addModule('domains');
