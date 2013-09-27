bdb2a34e8e9bf4960f7dffc7339dcae4136fcfff
Maciej Brencz <maciej.brencz@gmail.com>
2013-09-27 19:38:39 +0200

    domains: simplify unit test

--- test/modules/domains-test.js
+++ test/modules/domains-test.js
@@ -6,27 +6,26 @@ var vows = require('vows'),
 	mock = require('./mock');
 
 vows.describe('domains').addBatch({
-	'metrics are properly calculated': {
-		topic: function() {
-			var domains = [],
-				phantomas = mock.initModule('domains');
+	'module': mock.getContext('domains', function(phantomas) {
+		var domains = [];
 
-			domains.push({name: 'example.com', cnt: 2});
-			domains.push({name: 'awesome.cdn.com', cnt: 6});
-			domains.push({name: 'ads.co.uk', cnt: 3});
+		domains.push({name: 'example.com', cnt: 2});
+		domains.push({name: 'awesome.cdn.com', cnt: 6});
+		domains.push({name: 'ads.co.uk', cnt: 3});
 
-			domains.forEach(function(domain) {
-				for (var i=0; i<domain.cnt; i++) {
-					phantomas.recv({
-						domain: domain.name
-					});
-				}
-			});
+		domains.forEach(function(domain) {
+			for (var i=0; i<domain.cnt; i++) {
+				phantomas.recv({
+					domain: domain.name
+				});
+			}
+		});
 
-			// calculate metrics
-			return phantomas.report();
-		},
-		'domains': mock.assertMetric('domains', 3),
-		'maxRequestsPerDomain': mock.assertMetric('maxRequestsPerDomain', 6)
-	}
+		// calculate metrics
+		return phantomas.report();
+	},
+	{
+		'domains': 3,
+		'maxRequestsPerDomain': 6
+	})
 }).export(module);
