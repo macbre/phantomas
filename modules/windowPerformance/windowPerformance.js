128e5ee31f0cb4484d52b690f1f699f88bd6dedf
Maciej Brencz <maciej.brencz@gmail.com>
2013-11-05 20:08:14 +0100

    windowPerformance: relative to responseEnd
    onDOMReadyTime and windowOnLoadTime should be calculated relative to the first response being received

--- modules/windowPerformance/windowPerformance.js
+++ modules/windowPerformance/windowPerformance.js
@@ -6,23 +6,35 @@
 exports.version = '0.4';
 
 exports.module = function(phantomas) {
+	// times below are calculated relative to performance.timing.responseEnd (#117)
 	phantomas.setMetric('onDOMReadyTime');       // i.e. Navigation Timing - domContentLoadedEventStart and domComplete
 	phantomas.setMetric('onDOMReadyTimeEnd');    // i.e. Navigation Timing - domContentLoadedEventEnd
 	phantomas.setMetric('windowOnLoadTime');     // i.e. Navigation Timing - loadEventStart
 	phantomas.setMetric('windowOnLoadTimeEnd');  // i.e. Navigation Timing - loadEventEnd
 
 	// emulate window.performance
+	var responseEndTime;
+
+	// measure onDOMReadyTime and windowOnLoadTime from the moment HTML response was fully received
+	phantomas.once('responseEnd', function() {
+		responseEndTime = (new Date()).getTime();
+		phantomas.log('Performance timing: responseEnd');
+
+		phantomas.evaluate(function(responseEndTime) {
+			window.__phantomas.set('responseEndTime', responseEndTime);
+		}, responseEndTime);
+	});
+
 	phantomas.on('init', function() {
 		phantomas.evaluate(function() {
 			(function(phantomas) {
-				var start = Date.now();
-
 				phantomas.spyEnabled(false, 'installing window.performance metrics');
 
 				// emulate Navigation Timing
 				document.addEventListener('readystatechange', function() {
 					var readyState = document.readyState,
-						time = Date.now() - start,
+						responseEndTime = phantomas.get('responseEndTime'),
+						time = Date.now() - responseEndTime,
 						metricName;
 
 					// @see http://www.w3.org/TR/html5/dom.html#documentreadystate
@@ -38,6 +50,7 @@ exports.module = function(phantomas) {
 							break;
 
 						default:
+							phantomas.log('Performance timing: unhandled "' + readyState + '" state!');
 							return;
 					}
 
@@ -46,7 +59,7 @@ exports.module = function(phantomas) {
 
 					// measure when event handling is completed
 					setTimeout(function() {
-						var time = Date.now() - start;
+						var time = Date.now() - responseEndTime;
 
 						phantomas.setMetric(metricName + 'End', time);
 						phantomas.log('Performance timing: "' + readyState + '" state handling completed after ' + time + ' ms');
@@ -59,18 +72,14 @@ exports.module = function(phantomas) {
 	});
 
 	// fallback for --disable-js mode
-	var start;
-
-	phantomas.once('recv', function() {
-		start = (new Date()).getTime();
-	});
-
 	phantomas.on('loadFinished', function() {
-		var time = (new Date()).getTime() - start;
+		var time = (new Date()).getTime() - responseEndTime;
 
-		if (phantomas.getMetric('windowOnLoadTime') === 0) {
+		if (phantomas.getMetric('onDOMReadyTime') === 0) {
 			phantomas.setMetric('windowOnLoadTime', time);
 			phantomas.setMetric('windowOnLoadTimeEnd', time);
+
+			phantomas.log('Performance timing: document reached "complete" state after ' + time + ' ms (no JS fallback)');
 		}
 	});
 
