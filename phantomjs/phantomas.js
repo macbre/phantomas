65a45dfac1774ad800658d2837f61c3be4940126
Laurent Goderre <laurent.goderre@gmail.com>
2013-12-03 10:49:29 -0500

    Modified to use the local phantomjs instead of the global one.
    See (#112)

---
+++ phantomjs/phantomas.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env phantomjs
+/**
+ * PhantomJS-based web performance metrics collector
+ *
+ * @see https://github.com/macbre/phantomas
+ *
+ * Usage:
+ *  ./phantomas.js
+ *    --url=<page to check>
+ *    [--format=json|csv|tap|plain]
+ *    [--timeout=5]
+ *    ]--viewport=<width>x<height>]
+ *    [--verbose]
+ *    [--silent]
+ *    [--log=<log file>]
+ *    [--modules=moduleOne,moduleTwo]
+ *    [--skip-modules=moduleOne,moduleTwo]
+ *    [--user-agent='Custom user agent']
+ *    [--config='JSON config file']
+ *    [--cookie='bar=foo;domain=url']
+ *    [--no-externals]
+ *    [--allow-domain='domain,domain']
+ *    [--block-domain='domain,domain']
+ *    [--disable-js]
+ *    [--analyze-css]
+ *    [--film-strip]
+ */
+var args = require('system').args,
+        // parse script arguments
+        params = require('../lib/args').parse(args),
+        phantomas = require('../core/phantomas'),
+        instance;
+
+// compatibility layer for NodeJS modules
+process = {argv: []};
+
+// run phantomas
+instance = new phantomas(params);
+
+try {
+	instance.run();
+}
+catch(ex) {
+	console.log('phantomas v' + phantomas.version + ' failed with an error:');
+	console.log(ex);
+
+	phantom.exit(255);
+}
