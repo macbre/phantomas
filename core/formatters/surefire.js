2fc092a4fbeb9c68c88e4545b45b1d7e9a3b2f40
another-dave <whatsyourfavouritehummingnoise+github@gmail.com>
2013-12-05 15:52:07 +0000

    Adding additional objects to break out messages from the formatting function itself. Swapped if/else to simplify logic

--- core/formatters/surefire.js
+++ core/formatters/surefire.js
@@ -7,6 +7,7 @@
  * Results formatter for --format=surefire
  */
 module.exports = function (results) {
+
     var parseResults = function (results) {
         var metrics = results.getMetricsNames(),
             testSuite = {
@@ -15,25 +16,24 @@ module.exports = function (results) {
                 skippedCount: 0,
                 totalCount: metrics.length,
                 tests: []
-            }
-
-        metrics.forEach(function (metric) {
-            var test = {
-                name: metric
             };
 
-            // check asserts
-            if (results.hasAssertion(metric)) {
-                if (!results.assert(metric)) {
-                    testSuite.failedCount++;
-                    test.isOk = false;
-                    test.expectedResult = results.getAssertion(metric);
-                    test.actualResult = results.getMetric(metric);
-                }
-            } else {
-                // mark metrics with no assertions as skipped
+        metrics.forEach(function (metric) {
+            var testWasSkipped = !results.hasAssertion(metric),
+                testFailed = !results.assert(metric),
+                test = {
+                    name: metric
+                };
+
+            if (testWasSkipped) {
                 test.hasBeenRun = false;
                 testSuite.skippedCount++;
+
+            } else if (testFailed) {
+                testSuite.failedCount++;
+                test.isOk = false;
+                test.expectedResult = results.getAssertion(metric);
+                test.actualResult = results.getMetric(metric);
             }
 
             testSuite.tests.push(test);
@@ -43,51 +43,73 @@ module.exports = function (results) {
 
 
     var parseProperties = function (results) {
-        return [];
+        return [{name: "version", value: results.getGenerator()}];
     }
 
     var formattedResults = function (testSuite, properties) {
-        var outputLines = [];
-        outputLines.push('<?xml version="1.0" encoding="UTF-8" ?>');
-        outputLines.push(['<testsuite',
-								'failures="' + testSuite.failedCount + '"',
-								'time="0"',
-								'errors="0"',
-								'skipped="' + testSuite.skippedCount + '"',
-								'tests="' + testSuite.totalCount + '"',
-								'name="' + testSuite.name + '"',
-        				'>'].join(' '));
+        var outputLines = [],
+            NEW_LINE = '\n',
+            TESTSUITE = {
+                xml: '<?xml version="1.0" encoding="UTF-8" ?>',
+                startTag: function (failed, skipped, total, name) {
+                    return '<testsuite failures="' + failed + '" time="0" errors="0" skipped="' +
+                        skipped + '" tests="' + total + '" name="' + name + '">';
+                },
+                endTag: '</testsuite>'
+            },
+            TESTCASE = {
+                startTag: function (test) {
+                    var startTag = '\t<testcase time="0" classname="phantomas.asserts" name="' + test.name + '"';
+                    startTag += (test.isOk && test.hasBeenRun) ? '/>' : '>';
+                    return startTag;
+                },
+                endTag: '\t</testcase>'
+            },
+            FAILURE = {
+                startTag: '\t\t<failure type="phantomas.asserts.AssertionFailedError">',
+                endTag: '\t\t</failure>',
+                message: function (name, expected, actual) {
+                    return '\t\t\tExpected value for ' + name + ' to be "' + expected + '", but was "' + actual + '".';
+                }
+            },
+            PROPERTIES = {
+                startTag: '\t<properties>',
+                endTag: '\t</properties>',
+                node: function (name, value) {
+                    return '\t\t<property name="' + name + '" value="' + value + '"/>\n\t'
+                }
+            },
+            SKIPPED = '\t\t<skipped/>';
+            
+        outputLines.push(TESTSUITE.xml);
+        outputLines.push(TESTSUITE.startTag(testSuite.failedCount, testSuite.skippedCount,
+            testSuite.totalCount, testSuite.name));
 
         if (properties.length > 0) {
-            outputLines.push('\t<properties>');
+            outputLines.push(PROPERTIES.startTag);
             properties.forEach(function (property) {
-                outputLines.push('\t\t<property name="' + property.name + '" value="' + property.value + '"/>\n\t');
+                outputLines.push(PROPERTIES.node(property.name, property.value));
             });
-            outputLines.push('\t</properties>');
+            outputLines.push(PROPERTIES.endTag);
+            outputLines.push();
         }
 
         testSuite.tests.forEach(function (test) {
-            outputLines.push(['\t<testcase',
-                				'time="0"',
-				                'classname="phantomas.asserts"',
-                				'name="' + test.name + '"',
-				                (test.isOk && test.hasBeenRun) 
-				                	? '/>' 
-				                	: '>',].join(' '));
+            outputLines.push(TESTCASE.startTag(test));
 
             if (test.expectedResult && test.actualResult) {
-                outputLines.push('\t\t<failure type="phantomas.asserts.AssertionFailedError">');
-                outputLines.push('\t\t\tExpected value for ' + test.name + ' to be "' + test.expectedResult + '", but was "' + test.actualResult + '".');
-                outputLines.push('\t\t</failure>');
-                outputLines.push('\t</testcase>');
+                outputLines.push(FAILURE.startTag);
+                outputLines.push(FAILURE.message(test.name, test.expectedResult, test.actualResult));
+                outputLines.push(FAILURE.endTag);
+                outputLines.push(TESTCASE.endTag);
 
             } else if (!test.hasBeenRun) {
-                outputLines.push('\t\t<skipped/>');
-                outputLines.push('\t</testcase>');
+                outputLines.push(SKIPPED);
+                outputLines.push(TESTCASE.endTag);
             }
         });
-        outputLines.push('</testsuite>');
-        return outputLines.join('\n').trim();
+        outputLines.push(TESTSUITE.endTag);
+        return outputLines.join(NEW_LINE).trim();
     }
 
     // public API
