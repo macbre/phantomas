93bfef4e804ac9af0181ed340b1a452dba0d4a99
Maciej Brencz <maciej.brencz@gmail.com>
2013-12-07 21:19:38 +0100

    screenshot: handle --screenshot option

---
+++ modules/screenshot/screenshot.js
@@ -0,0 +1,32 @@
+/**
+ * Renders a screenshot of the page when it's fully loaded
+ *
+ * @see http://phantomjs.org/api/webpage/method/render.html
+ */
+exports.version = '0.1';
+
+exports.module = function(phantomas) {
+	var param = phantomas.getParam('screenshot'),
+		path = '';
+
+	if (typeof param === 'undefined') {
+		return;
+	}
+
+	// --screenshot
+	if (param === true) {
+		// default to "2013-12-07T20:15:01.521Z.png"
+		path = (new Date()).toJSON() + '.png';
+	}
+	// --screenshot [file name]
+	else {
+		path = param;
+	}
+
+	phantomas.on('report', function() {
+		var then = Date.now();
+		phantomas.render(path);
+
+		phantomas.log('Screenshot: rendered to %s in %d ms', path, Date.now() - then);
+	});
+};
