6e5060d54bb98d2465bd423b3f9c88d0607c182d
Kenny Dits <kenny.dits@m6.fr>
2013-12-14 10:28:49 +0100

    fix lint - add offender for cache passes - remove one toLowerCase call

--- modules/cacheHits/cacheHits.js
+++ modules/cacheHits/cacheHits.js
@@ -14,26 +14,27 @@ exports.module = function(phantomas) {
 	// X-Cache:HIT, HIT
 	// X-Cache:arsenic miss (0)
 	phantomas.on('recv', function(entry,res) {
-		var header = entry.headers['X-Cache'] || '',
+		var header = entry.headers['X-Cache'].toLowerCase() || '',
 			isHit,
 			isPass;
 
 		if (re.test(header)) {
-		   isHit = header.toLowerCase().indexOf('hit') > -1;
-		   if (isHit) {
-		        phantomas.incrMetric('cacheHits');
-		   }
-		   else {
-		        isPass = header.toLowerCase().indexOf('pass') > -1;
-		        if (isPass) {
-		             phantomas.incrMetric('cachePasses');
-		             phantomas.log('Cache pass: on <' + entry.url + '> (X-Cache: ' + header + ')');
-		        }
-		        else {
-		             phantomas.incrMetric('cacheMisses');
-		             phantomas.log('Cache miss: on <' + entry.url + '> (X-Cache: ' + header + ')');
-		        }
-		   }
+			isHit = header.indexOf('hit') > -1;
+			if (isHit) {
+				phantomas.incrMetric('cacheHits');
+			}
+			else {
+				isPass = header.indexOf('pass') > -1;
+				if (isPass) {
+					phantomas.incrMetric('cachePasses');
+					phantomas.addOffender('cachePasses', entry.url);
+					phantomas.log('Cache pass: on <' + entry.url + '> (X-Cache: ' + header + ')');
+				}
+				else {
+					phantomas.incrMetric('cacheMisses');
+					phantomas.log('Cache miss: on <' + entry.url + '> (X-Cache: ' + header + ')');
+				}
+			}
 		}
 	});
 };
