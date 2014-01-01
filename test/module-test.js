9b1992fc06439c7c20cd4934c709d3e363d4fd90
Maciej Brencz <maciej.brencz@gmail.com>
2014-01-01 22:07:24 +0100

    Add tests for CommonJS module
    Improves #138

---
+++ test/module-test.js
@@ -0,0 +1,42 @@
+/**
+ * Tests CommonJS module
+ */
+'use strict';
+
+var vows = require('vows'),
+	assert = require('assert'),
+	phantomas = require('..');
+
+// run the test
+vows.describe('CommonJS module').addBatch({
+	'when not provided with URL': {
+		topic: function() {
+			phantomas(false, this.callback);
+		},
+		'should fail with err #255': function(err, stats) {
+			assert.equal(err, 255);
+		}
+	},
+	'when timed out': {
+		topic: function() {
+			phantomas('http://phantomjs.org/', {timeout: 1}, this.callback);
+		},
+		'should fail with err #252': function(err, stats) {
+			assert.equal(err, 252);
+		}
+	},
+	'for a valid URL': {
+		topic: function() {
+			phantomas('http://example.com/', this.callback);
+		},
+		'should not throw an error': function(err, stats) {
+			assert.strictEqual(err, null);
+		},
+		'should return metrics and other stats': function(err, stats) {
+			assert.equal(stats.url, 'http://example.com/');
+			assert.equal(typeof stats.generator, 'string');
+			assert.equal(typeof stats.metrics, 'object');
+			assert.equal(typeof stats.offenders, 'object');
+		}
+	}
+}).export(module);
