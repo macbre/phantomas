7f5ee1f0e745b77321989e798fb2eb0aab4e6911
Maciej Brencz <maciej.brencz@gmail.com>
2014-01-12 21:02:35 +0100

    redirects: add redirectsTime metric

--- modules/redirects/redirects.js
+++ modules/redirects/redirects.js
@@ -5,12 +5,15 @@ exports.version = '0.1';
 
 exports.module = function(phantomas) {
 	phantomas.setMetric('redirects');
+	phantomas.setMetric('redirectsTime');
 
 	phantomas.on('recv', function(entry, res) {
 		if (entry.isRedirect) {
 			phantomas.incrMetric('redirects');
+			phantomas.incrMetric('redirectsTime', entry.timeToLastByte);
+
 			phantomas.addOffender('redirects', entry.url + ' is a redirect (HTTP ' + entry.status + ' ' + entry.statusText + ') ' +
-				'to ' + (res.redirectURL || entry.headers.Location));
+				'to ' + (res.redirectURL || entry.headers.Location) + ' (took ' + entry.timeToLastByte + ' ms)');
 		}
 	});
 };
--- test/modules/redirects-test.js
+++ test/modules/redirects-test.js
@@ -8,16 +8,18 @@ var vows = require('vows'),
 vows.describe('redirects').
 	addBatch({
 		'HTTP 301/302': mock.getContext('redirects', function(phantomas) {
-			return phantomas.recv({isRedirect: true, headers: {}}).report();
+			return phantomas.recv({isRedirect: true, timeToLastByte: 20, headers: {}}).report();
 		},
 		{
-			'redirects': 1
+			'redirects': 1,
+			'redirectsTime': 20
 		}),
 		'HTTP 200': mock.getContext('redirects', function(phantomas) {
 			return phantomas.recv().report();
 		},
 		{
-			'redirects': 0
+			'redirects': 0,
+			'redirectsTime': 0
 		})
 	}).
 	export(module);
