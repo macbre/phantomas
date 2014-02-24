803131e7bf3d0bc6647cee9f3b43a64dfe4da0eb
William Pain <pain.william@gmail.com>
2014-02-24 18:08:59 +0100

    Add HAR module, first release

---
+++ modules/har/har.js
@@ -0,0 +1,156 @@
+/**
+* Log requests for build HAR output
+* 
+* @see: https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/HAR/Overview.html
+*/
+
+var VERSION = '0.1';
+exports.version = VERSION;
+
+/**
+ * From netsniff.js with small workaround
+ * 
+ * @see: https://github.com/ariya/phantomjs/blob/master/examples/netsniff.js
+ */
+
+if (!Date.prototype.toISOString) {
+    Date.prototype.toISOString = function () {
+        function pad(n) { return n < 10 ? '0' + n : n; }
+        function ms(n) { return n < 10 ? '00'+ n : n < 100 ? '0' + n : n }
+        return this.getFullYear() + '-' +
+            pad(this.getMonth() + 1) + '-' +
+            pad(this.getDate()) + 'T' +
+            pad(this.getHours()) + ':' +
+            pad(this.getMinutes()) + ':' +
+            pad(this.getSeconds()) + '.' +
+            ms(this.getMilliseconds()) + 'Z';
+    }
+}
+
+function createHAR(address, title, startTime, endTime, resources)
+{
+    var entries = [];
+
+    resources.forEach(function (resource) {
+        var request = resource.request,
+            startReply = resource.startReply,
+            endReply = resource.endReply;
+
+        if (!request || !startReply || !endReply) {
+            return;
+        }
+
+        // Exclude Data URI from HAR file because
+        // they aren't included in specification
+        if (request.url.match(/(^data:image\/.*)/i)) {
+            return;
+    }
+
+        entries.push({
+            startedDateTime: request.time.toISOString(),
+            time: endReply.time - request.time,
+            request: {
+                method: request.method,
+                url: request.url,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: request.headers,
+                queryString: [],
+                headersSize: -1,
+                bodySize: -1
+            },
+            response: {
+                status: endReply.status,
+                statusText: endReply.statusText,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: endReply.headers,
+                redirectURL: "",
+                headersSize: -1,
+                bodySize: startReply.bodySize,
+                content: {
+                    size: startReply.bodySize,
+                    mimeType: endReply.contentType
+                }
+            },
+            cache: {},
+            timings: {
+                blocked: 0,
+                dns: -1,
+                connect: -1,
+                send: 0,
+                wait: startReply.time - request.time,
+                receive: endReply.time - startReply.time,
+                ssl: -1
+            },
+            pageref: address
+        });
+    });
+
+    return {
+        log: {
+            version: '1.2',
+            creator: {
+                name: "Phantomas - HAR",
+                version: VERSION
+            },
+            pages: [{
+                startedDateTime: startTime.toISOString(),
+                id: address,
+                title: title,
+                pageTimings: {
+                    onLoad: endTime.getTime() - startTime.getTime()
+                }
+            }],
+            entries: entries
+        }
+    };
+}
+/** End **/
+
+exports.module = function(phantomas) {
+    phantomas.setMetric('har');
+
+    var resources = [];
+    var startTime;
+    var endTime;
+    var page;
+
+    phantomas.on('pageBeforeOpen', function(p) {
+        page = p;
+    });
+
+    phantomas.on('pageOpen', function() {
+        startTime = new Date(); 
+    });
+
+    phantomas.on('loadFinished', function() {
+        endTime = new Date();
+    });
+
+    phantomas.on('onResourceRequested', function(res, req) {
+        resources[res.id] = {
+            request: res,
+            startReply: null,
+            endReply: null
+        };
+
+    });
+
+    phantomas.on('onResourceReceived', function(res) {
+        if (res.stage === 'start')
+            resources[res.id].startReply = res;
+
+        if (res.stage === 'end')
+            resources[res.id].endReply = res;
+    });
+
+    phantomas.on('report', function() {
+        var address = page.url;
+        var title = page.title;
+
+        var har = createHAR(address, title, startTime, endTime, resources);
+
+        phantomas.setMetric('har', har);
+    });
+};
