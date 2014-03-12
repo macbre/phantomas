macbre/phantomas

commit b070626b03c1b2f958fd98f2c1edc6f19c5dfa9a

Pass BW=1 env variable when needed

lib/index.js
@@ -13,6 +13,9 @@ function phantomas(url, options, callback) {
 		events = new emitter(),
 		path = '',
 		proc,
+		spawnOptions = {
+			env: {}
+		},
 		data = '';
 
 	// options can be omitted
@@ -34,6 +37,12 @@ function phantomas(url, options, callback) {
 	// build path to PhantomJS
 	path = phantomjs.path;
 	args.push(__dirname + '/../scripts/phantomas.js');
+
+	// add env variable to turn off ANSI colors when needed (#237)
+	if (!process.stdout.isTTY) {
+		debug('ANSI colors turned off');
+		spawnOptions.env.BW = 1;
+	}
 
 	// build args
 	Object.keys(options).forEach(function(key) {
@@ -70,7 +79,7 @@ function phantomas(url, options, callback) {
 	debug('Running %s %s', path, args.join(' '));
 
 	// spawn the process
-	proc = spawn(path, args);
+	proc = spawn(path, args, spawnOptions);
 
 	debug('Spawned with pid #%d', proc.pid);
 

commit db3a8bcdcff1aa4978b4e17c9237ac6030fe587a

Pass envirnoment variables when running PhantomJS
Fixes /usr/bin/env: node: No such file or directory

Regression introduced by b070626b

lib/index.js
@@ -15,7 +15,7 @@ function phantomas(url, options, callback) {
 		path = '',
 		proc,
 		spawnOptions = {
-			env: {}
+			env: process.env
 		},
 		data = '';
 