66264de153d0a45c2a0bd9bb426ce9baef79e9b1
Vladislav Gangan <vgangan@tacitknowledge.com>
2014-03-26 15:35:12 +1100

    Capturing the trail of HTTP status codes for the main request

---
+++ modules/mainRequest/mainRequest.js
@@ -0,0 +1,25 @@
+/**
+ * Analyzes bits of data pertaining to the main request only
+ */
+exports.version = '0.1';
+
+exports.module = function(phantomas) {
+    var isMainRequest = true;
+
+    phantomas.setMetric('statusCodesTrail', []); // @desc list of HTTP status codes that main request followed through
+
+	phantomas.on('recv', function(entry, res) {
+        if (isMainRequest) {
+            captureStatusCode(res.status);
+        }
+	});
+
+    phantomas.on('responseEnd', function(entry, res) {
+        isMainRequest = false;
+        captureStatusCode(res.status);
+    });
+
+    function captureStatusCode(code) {
+        phantomas.getMetric('statusCodesTrail').push(code);
+    }
+};
---
+++ test/modules/mainRequest-test.js
@@ -0,0 +1,59 @@
+/**
+ * Tests mainRequest module
+ */
+var vows = require('vows'),
+    assert = require('assert'),
+    mock = require('./mock');
+
+vows.describe('mainRequest').
+    addBatch({
+        'redirect request': {
+            topic: function () {
+                return mock.initModule('mainRequest').
+                    recv({}, {status: 301}).
+                    responseEnd({}, {status: 200}).
+                    report();
+            },
+            'contains all status codes in the trail': function (topic) {
+                var metric = topic.getMetric('statusCodesTrail');
+                assert.deepEqual(metric, [301, 200])
+            }
+        },
+        'long redirect request': {
+            topic: function () {
+                return mock.initModule('mainRequest').
+                    recv({}, {status: 301}).
+                    recv({}, {status: 302}).
+                    responseEnd({}, {status: 404}).
+                    report();
+            },
+            'contains all status codes in the trail': function (topic) {
+                var metric = topic.getMetric('statusCodesTrail');
+                assert.deepEqual(metric, [301, 302, 404])
+            }
+        },
+        'non-redirect (e.g. terminal) first request': {
+            topic: function () {
+                return mock.initModule('mainRequest').
+                    responseEnd({}, {status: 200}).
+                    report();
+            },
+            'contains only its status code in the trail': function (topic) {
+                var metric = topic.getMetric('statusCodesTrail');
+                assert.deepEqual(metric, [200])
+            }
+        },
+        'multiple requests': {
+            topic: function () {
+                return mock.initModule('mainRequest').
+                    responseEnd({}, {status: 200}).
+                    recv({}, {status: 404}).
+                    report();
+            },
+            'capture only the first request in the trail': function (topic) {
+                var metric = topic.getMetric('statusCodesTrail');
+                assert.deepEqual(metric, [200])
+            }
+        }
+    }).
+    export(module);
