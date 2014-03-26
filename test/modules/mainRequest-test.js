7307f3c09b152c5333d5f8cd23c4bd10c9beeead
Vladislav Gangan <vgangan@tacitknowledge.com>
2014-03-26 21:54:34 +1100

    Making use of the syntactic sugar provided by mock.js since we do not deal with arrays anymore and as such it is possible to perform strict assertions

--- test/modules/mainRequest-test.js
+++ test/modules/mainRequest-test.js
@@ -7,53 +7,41 @@ var vows = require('vows'),
 
 vows.describe('mainRequest').
     addBatch({
-        'redirect request': {
-            topic: function () {
-                return mock.initModule('mainRequest').
+        'redirect request': mock.getContext('mainRequest', function (phantomas) {
+                return phantomas.
                     recv({}, {status: 301}).
                     responseEnd({}, {status: 200}).
                     report();
             },
-            'contains all status codes in the trail': function (topic) {
-                var metric = topic.getMetric('statusCodesTrail');
-                assert.deepEqual(metric, [301, 200]);
-            }
-        },
-        'long redirect request': {
-            topic: function () {
-                return mock.initModule('mainRequest').
+            {
+                'statusCodesTrail': '301,200'
+            }),
+        'long redirect request': mock.getContext('mainRequest', function (phantomas) {
+                return phantomas.
                     recv({}, {status: 301}).
                     recv({}, {status: 302}).
                     responseEnd({}, {status: 404}).
                     report();
             },
-            'contains all status codes in the trail': function (topic) {
-                var metric = topic.getMetric('statusCodesTrail');
-                assert.deepEqual(metric, [301, 302, 404]);
-            }
-        },
-        'non-redirect (e.g. terminal) first request': {
-            topic: function () {
-                return mock.initModule('mainRequest').
+            {
+                'statusCodesTrail': '301,302,404'
+            }),
+        'non-redirect (e.g. terminal) first request': mock.getContext('mainRequest', function (phantomas) {
+                return phantomas.
                     responseEnd({}, {status: 200}).
                     report();
             },
-            'contains only its status code in the trail': function (topic) {
-                var metric = topic.getMetric('statusCodesTrail');
-                assert.deepEqual(metric, [200]);
-            }
-        },
-        'multiple requests': {
-            topic: function () {
-                return mock.initModule('mainRequest').
+            {
+                'statusCodesTrail': '200'
+            }),
+        'multiple requests': mock.getContext('mainRequest', function (phantomas) {
+                return phantomas.
                     responseEnd({}, {status: 200}).
                     recv({}, {status: 404}).
                     report();
             },
-            'capture only the first request in the trail': function (topic) {
-                var metric = topic.getMetric('statusCodesTrail');
-                assert.deepEqual(metric, [200]);
-            }
-        }
+            {
+                'statusCodesTrail': '200'
+            })
     }).
     export(module);
