97fbf9ec4b8cfa32d53c2a538b5a84ce828a0184
Maciej Brencz <maciej.brencz@gmail.com>
2014-04-04 20:17:30 +0200

    Migrate staticAssets module to offender

--- modules/staticAssets/staticAssets.js
+++ modules/staticAssets/staticAssets.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-exports.version = '0.2';
+exports.version = '0.3';
 
 exports.module = function(phantomas) {
 	var BASE64_SIZE_THRESHOLD = 2 * 1024;
@@ -17,30 +17,36 @@ exports.module = function(phantomas) {
 	phantomas.setMetric('multipleRequests'); // @desc number of static assets that are requested more than once
 
 	phantomas.on('recv', function(entry, res) {
-		// console.log(JSON.stringify(entry));
+		//phantomas.log('entry: %j', entry);
 		var isContent = entry.status === 200;
+
+		// skip tracking requests
+		// TODO: use 3pc database with tracking services
+		if (/google-analytics.com\/__utm.gif|pixel.quantserve.com\/pixel/.test(entry.url)) {
+			return;
+		}
 
 		// check for query string -> foo.css?123
 		if (entry.isImage || entry.isJS || entry.isCSS) {
 			if (entry.url.indexOf('?') > -1) {
-				phantomas.log('Query string: <' + entry.url + '> (' + entry.type.toUpperCase() + ') served with query string');
 				phantomas.incrMetric('assetsWithQueryString');
+				phantomas.addOffender('assetsWithQueryString', entry.url + ' (' + entry.type.toUpperCase() + ')');
 			}
 		}
 
 		// check for not-gzipped CSS / JS / HTML files
 		if (entry.isJS || entry.isCSS || entry.isHTML) {
 			if (!entry.gzip && isContent) {
-				phantomas.log('GZIP: <' + entry.url + '> (' + entry.type.toUpperCase() + ') served without compression');
 				phantomas.incrMetric('assetsNotGzipped');
+				phantomas.addOffender('assetsNotGzipped', entry.url + ' (' + entry.type.toUpperCase() + ')');
 			}
 		}
 
 		// check small images that can be base64 encoded
 		if (entry.isImage) {
 			if (entry.bodySize < BASE64_SIZE_THRESHOLD) {
-				phantomas.log('base64: <' + entry.url + '> (' + (entry.bodySize/1024).toFixed(2) + ' kB) should be considered to be served base64-encoded');
 				phantomas.incrMetric('smallImages');
+				phantomas.addOffender('smallImages', entry.url + ' (' + (entry.bodySize/1024).toFixed(2) + ' kB)');
 			}
 		}
 
