c35410ceebc66a7318d27b0eea1b3db663c47b5b
Maciej Brencz <maciej.brencz@gmail.com>
2014-05-16 21:40:11 +0200

    csv: handle "no-header", "timestamp", "url" options

--- reporters/csv.js
+++ reporters/csv.js
@@ -1,5 +1,10 @@
 /**
- * Results formatter for --format=csv
+ * Results formatter for -R csv
+ *
+ * Options:
+ *  no-header - omit CSV header
+ *  timestamp - add the current timestamp as the first column
+ *  url - add the URL as the first column
  *
  * @see https://github.com/touv/node-csv-string
  */
@@ -7,13 +12,14 @@
 
 var CSV = require('csv-string');
 
-module.exports = function(results) {
+module.exports = function(results, reporterOptions) {
 	// public API
 	return {
 		render: function() {
 			var metrics = results.getMetricsNames(),
 				keys = [],
-				values = [];
+				values = [],
+				ret = [];
 
 			metrics.forEach(function(metric) {
 				var value = results.getMetric(metric);
@@ -22,7 +28,27 @@ module.exports = function(results) {
 				values.push(value);
 			});
 
-			return CSV.stringify([keys, values]);
+			// -R csv:url
+			if (reporterOptions.url === true) {
+				values.unshift(results.getUrl());
+				keys.unshift('url');
+			}
+
+			// -R csv:timestamp
+			if (reporterOptions.timestamp === true) {
+				values.unshift( new Date().toJSON().substr(0,19) );
+				keys.unshift('timestamp');
+			}
+
+			// -R csv:no-header
+			if (reporterOptions['no-header'] !== true) {
+				ret.push(keys);
+			}
+
+			// add the values
+			ret.push(values);
+
+			return CSV.stringify(ret);
 		}
 	};
 };
