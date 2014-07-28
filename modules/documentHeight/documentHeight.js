9bc93c439f21a7af60603876f4cd4a7ed7f74933
Maciej Brencz <maciej.brencz@gmail.com>
2014-07-28 22:13:27 +0200

    Add documentHeight metric

---
+++ modules/documentHeight/documentHeight.js
@@ -0,0 +1,22 @@
+/**
+ * Measure document height
+ */
+/* global document: true */
+'use strict';
+
+exports.version = '0.1';
+
+exports.module = function(phantomas) {
+	phantomas.setMetric('documentHeight'); // @desc the page height [px]
+
+	phantomas.on('report', function() {
+		phantomas.setMetricEvaluate('documentHeight', function() {
+			// @see https://github.com/HTTPArchive/httparchive/blob/master/custom_metrics/document_height.js
+			var doc = document,
+				body = doc.body,
+				docelem = doc.documentElement;
+
+			return Math.max( body.scrollHeight, body.offsetHeight, docelem.clientHeight, docelem.scrollHeight, docelem.offsetHeight );
+		});
+	});
+};
