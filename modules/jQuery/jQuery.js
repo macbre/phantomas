154d6c4d98fcaee8ae978aad52b9ed03da421aee
Maciej Brencz <maciej.brencz@gmail.com>
2014-12-03 22:18:29 +0100

    jQueryEventTriggers: add a metric (#440)

--- modules/jQuery/jQuery.js
+++ modules/jQuery/jQuery.js
@@ -13,37 +13,49 @@ exports.module = function(phantomas) {
 	phantomas.setMetric('jQueryVersion', ''); // @desc version of jQuery framework (if loaded) [string]
 	phantomas.setMetric('jQueryOnDOMReadyFunctions'); // @desc number of functions bound to onDOMReady event
 	phantomas.setMetric('jQuerySizzleCalls'); // @desc number of calls to Sizzle (including those that will be resolved using querySelectorAll)
+	phantomas.setMetric('jQueryEventTriggers'); // @desc number of jQuery event triggers
 
 	// spy calls to jQuery functions
 	phantomas.once('init', function() {
 		phantomas.evaluate(function() {
 			(function(phantomas) {
-				phantomas.spyGlobalVar('jQuery', function(val) {
+				phantomas.spyGlobalVar('jQuery', function(jQuery) {
 					var version;
 
-					if (!val || !val.fn) {
+					if (!jQuery || !jQuery.fn) {
 						phantomas.log('jQuery: unable to detect version!');
 						return;
 					}
 
-					version = val.fn.jquery;
+					version = jQuery.fn.jquery;
 
 					phantomas.log('jQuery: loaded v' + version);
 					phantomas.setMetric('jQueryVersion', version);
 
 					// jQuery.ready.promise
 					// works for jQuery 1.8.0+ (released Aug 09 2012)
-					phantomas.spy(val.ready, 'promise', function() {
+					phantomas.spy(jQuery.ready, 'promise', function() {
 						phantomas.incrMetric('jQueryOnDOMReadyFunctions');
 						phantomas.addOffender('jQueryOnDOMReadyFunctions', phantomas.getCaller(3));
 					}) || phantomas.log('jQuery: can not measure jQueryOnDOMReadyFunctions (jQuery used on the page is too old)!');
 
 					// Sizzle calls - jQuery.find
 					// works for jQuery 1.3+ (released Jan 13 2009)
-					phantomas.spy(val, 'find', function(selector, context) {
+					phantomas.spy(jQuery, 'find', function(selector, context) {
 						phantomas.incrMetric('jQuerySizzleCalls');
 						phantomas.addOffender('jQuerySizzleCalls', '%s (in %s)', selector, (phantomas.getDOMPath(context) || 'unknown'));
 					}) || phantomas.log('jQuery: can not measure jQuerySizzleCalls (jQuery used on the page is too old)!');
+
+					// jQuery events triggers (issue #440)
+					phantomas.spy(jQuery.event, 'trigger', function(ev, data, elem) {
+						var path = phantomas.getDOMPath(elem),
+							type = ev.type || ev;
+
+						phantomas.log('Event: triggered "%s" on "%s"', type, path);
+
+						phantomas.incrMetric('jQueryEventTriggers');
+						phantomas.addOffender('jQueryEventTriggers', '"%s" on "%s"', type, path);
+					});
 				});
 			})(window.__phantomas);
 		});
