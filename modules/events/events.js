5d956ee93e7a14b816a349ad9b062623e2cd4a51
Maciej Brencz <maciej.brencz@gmail.com>
2014-12-04 19:30:07 +0100

    events: rename from eventListeners and introduce "eventsDispatched" metric (#440)

--- modules/events/events.js
+++ modules/events/events.js
@@ -4,16 +4,17 @@
 /* global Document: true, Element: true, window: true */
 'use strict';
 
-exports.version = '0.2';
+exports.version = '0.3';
 
 exports.module = function(phantomas) {
 	phantomas.setMetric('eventsBound'); // @desc number of EventTarget.addEventListener calls
+	phantomas.setMetric('eventsDispatched'); // @desc number of EventTarget.dispatchEvent calls
 
-	// spy calls to EventTarget.addEventListener
-	// @see https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.addEventListener
 	phantomas.once('init', function() {
 		phantomas.evaluate(function() {
 			(function(phantomas) {
+				// spy calls to EventTarget.addEventListener
+				// @see https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.addEventListener
 				function eventSpy(eventType) {
 					/* jshint validthis: true */
 					var path = phantomas.getDOMPath(this);
@@ -26,6 +27,18 @@ exports.module = function(phantomas) {
 				phantomas.spy(Element.prototype, 'addEventListener', eventSpy);
 				phantomas.spy(Document.prototype, 'addEventListener', eventSpy);
 				phantomas.spy(window, 'addEventListener', eventSpy);
+
+				// spy calls to EventTarget.dispatchEvent
+				// @see https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.dispatchEvent
+				phantomas.spy(Element.prototype, 'dispatchEvent', function(ev) {
+					/* jshint validthis: true */
+					var path = phantomas.getDOMPath(this);
+
+					phantomas.log('Core JS event: triggered "%s" on "%s"', ev.type, path);
+
+					phantomas.incrMetric('eventsDispatched');
+					phantomas.addOffender('eventsDispatched', '"%s" on "%s"', ev.type, path);
+				});
 			})(window.__phantomas);
 		});
 	});
