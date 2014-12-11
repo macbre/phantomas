e60e29cc617a845f84ea563f6faafdbc19353fb6
Maciej Brencz <maciej.brencz@gmail.com>
2014-12-11 19:30:33 +0100

    screenshots: use valid Windows characters in default filename (fixes #454)

--- extensions/screenshot/screenshot.js
+++ extensions/screenshot/screenshot.js
@@ -5,20 +5,24 @@
  */
 'use strict';
 
-exports.version = '0.1';
+exports.version = '0.2';
 
 exports.module = function(phantomas) {
 	var param = phantomas.getParam('screenshot'),
 		path = '';
 
 	if (typeof param === 'undefined') {
+		phantomas.log('Screenshot: to enable screenshot of the fully loaded page run phantomas with --screenshot option');
 		return;
 	}
 
 	// --screenshot
 	if (param === true) {
 		// defaults to "2013-12-07T20:15:01.521Z.png"
-		path = (new Date()).toJSON() + '.png';
+		path = (new Date()).toJSON().
+		replace(/:/g, '-'); // be M$ Windows compatible (issue #454)
+
+		path += '.png';
 	}
 	// --screenshot [file name]
 	else {
@@ -26,9 +30,14 @@ exports.module = function(phantomas) {
 	}
 
 	phantomas.on('report', function() {
-		var then = Date.now();
+		var then = Date.now(),
+			time;
+
 		phantomas.render(path);
 
-		phantomas.log('Screenshot: rendered to %s in %d ms', path, Date.now() - then);
+		time = Date.now() - then;
+
+		phantomas.log('Screenshot: rendered to %s in %d ms', path, time);
+		phantomas.emit('screenshot', path, time);
 	});
 };
