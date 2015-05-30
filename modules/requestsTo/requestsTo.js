48489798c9b0a7509a83c70522b185f60c347ca7
Maciej Brencz <maciej.brencz@gmail.com>
2015-05-30 17:54:15 +0200

    More requetsTo metrics
    Resolves #486

--- modules/requestsTo/requestsTo.js
+++ modules/requestsTo/requestsTo.js
@@ -3,29 +3,54 @@
  */
 'use strict';
 
-exports.version = '0.1';
+exports.version = '1.0';
 
 exports.module = function(phantomas) {
+	phantomas.setMetric('requestsToFirstPaint'); // @desc number of HTTP requests it took to make the first paint @gecko
+	phantomas.setMetric('domainsToFirstPaint'); // @desc number of domains used to make the first paint @offenders @gecko @offenders
 	phantomas.setMetric('requestsToDomContentLoaded'); // @desc number of HTTP requests it took to make the page reach DomContentLoaded state
+	phantomas.setMetric('domainsToDomContentLoaded'); // @desc number of domains used to make the page reach DomContentLoaded state @offenders
 	phantomas.setMetric('requestsToDomComplete'); // @desc number of HTTP requests it took to make the page reach DomComplete state
+	phantomas.setMetric('domainsToDomComplete'); // @desc number of domains used to make the page reach DomComplete state @offenders
 
-	var requests = 0;
+	var Collection = require('../../lib/collection'),
+		domains = new Collection(),
+		requests = 0;
+
+	function setDomainMetric(metricName) {
+		phantomas.setMetric(metricName, domains.getLength());
+		domains.sort().forEach(function(domain, cnt) {
+			phantomas.addOffender(metricName, '%s (%d requests)', domain, cnt);
+		});
+	}
 
 	phantomas.on('recv', function(entry, res) {
+		//phantomas.log('requestsTo: #%d <%s> / %s', requests, entry.url, entry.domain);
+
 		requests++;
-		//phantomas.log('requestsTo: #%d <%s>', requests, entry.url);
+
+		if (entry.domain) {
+			domains.push(entry.domain);
+		}
 	});
 
 	phantomas.on('milestone', function(name) {
 		//phantomas.log('requestsTo: %s (after %d requests)', name, requests);
 
 		switch (name) {
+			case 'firstPaint':
+				phantomas.setMetric('requestsToFirstPaint', requests);
+				setDomainMetric('domainsToFirstPaint');
+				break;
+
 			case 'domInteractive':
 				phantomas.setMetric('requestsToDomContentLoaded', requests);
+				setDomainMetric('domainsToDomContentLoaded');
 				break;
 
 			case 'domComplete':
 				phantomas.setMetric('requestsToDomComplete', requests);
+				setDomainMetric('domainsToDomComplete');
 				break;
 		}
 	});
