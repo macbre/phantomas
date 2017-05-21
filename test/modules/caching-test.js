fc83af49a9558efea6ab0d122e161774569d1afb
Maciej Brencz <maciej.brencz@gmail.com>
2017-05-21 14:04:33 +0200

    caching: add test cases

---
+++ test/modules/caching-test.js
@@ -0,0 +1,64 @@
+/**
+ * Test caching module
+ */
+var vows = require('vows'),
+	assert = require('assert'),
+	mock = require('./mock');
+
+vows.describe('caching').
+addBatch({
+	'caching not specified': mock.getContext('caching', function(phantomas) {
+		return phantomas.recv({
+			isImage: true,
+			headers: {
+				'X-Foo': 'Bar'
+			}
+		}).report();
+	}, {
+		'cachingNotSpecified': 1
+	}),
+	'old caching header used (Expires)': mock.getContext('caching', function(phantomas) {
+		return phantomas.recv({
+			isImage: true,
+			headers: {
+				'Expires': 'Wed, 21 Oct 2015 07:28:00 GMT'
+			}
+		}).report();
+	}, {
+		'oldCachingHeaders': 1,
+		'cachingNotSpecified': 0,
+		'cachingDisabled': 1,
+	}),
+	'old caching header used (Pragma)': mock.getContext('caching', function(phantomas) {
+		return phantomas.recv({
+			isImage: true,
+			headers: {
+				'Pragma': 'no-cache'
+			}
+		}).report();
+	}, {
+		'oldCachingHeaders': 1,
+		'cachingNotSpecified': 1
+	}),
+	'caching too short': mock.getContext('caching', function(phantomas) {
+		return phantomas.recv({
+			isImage: true,
+			headers: {
+				'Cache-Control': 'max-age=600'
+			}
+		}).report();
+	}, {
+		'cachingTooShort': 1
+	}),
+	'caching not too short': mock.getContext('caching', function(phantomas) {
+		return phantomas.recv({
+			isImage: true,
+			headers: {
+				'Cache-Control': 'max-age=2592000' // 30 days
+			}
+		}).report();
+	}, {
+		'cachingTooShort': 0
+	}),
+}).
+export(module);
