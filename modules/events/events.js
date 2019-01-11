a04766fe68b11bda4a540cbf29d6fd6b0fad6589
macbre <maciej.brencz@gmail.com>
2019-01-11 21:11:35 +0100

    events: module ported
     /jquery.html
        ✓ should be generated
        ✓ should have "jQueryVersion" metric properly set
        ✓ should have "jQueryOnDOMReadyFunctions" metric properly set
        ✓ should have "jQueryWindowOnLoadFunctions" metric properly set
        ✓ should have "jQuerySizzleCalls" metric properly set
        ✓ should have "jQueryEventTriggers" metric properly set
        ✓ should have "eventsBound" metric properly set
        ✓ should have "eventsDispatched" metric properly set
        ✓ should have "eventsScrollBound" metric properly set
        ✓ should have "eventsDispatched" offender(s) properly set
        ✓ should have "eventsBound" offender(s) properly set
        ✓ should have "eventsScrollBound" offender(s) properly set
        ✓ should have "jQueryVersionsLoaded" offender(s) properly set
        ✓ should have "jQueryWindowOnLoadFunctions" offender(s) properly set
        ✓ should have "jQueryEventTriggers" offender(s) properly set
        ✓ should have "jQuerySizzleCalls" offender(s) properly set
        ✓ should have "jQueryDOMReads" offender(s) properly set
        ✓ should have "jQueryDOMWrites" offender(s) properly set
        ✓ should have "jQueryDOMWriteReadSwitches" offender(s) properly set

--- modules/events/events.js
+++ modules/events/events.js
@@ -9,44 +9,6 @@ module.exports = function(phantomas) {
 	phantomas.setMetric('eventsDispatched'); // @desc number of EventTarget.dispatchEvent calls
 	phantomas.setMetric('eventsScrollBound'); // @desc number of scroll event bounds
 
-	return; // TODO
-
-	phantomas.on('init', function() {
-		phantomas.evaluate(function() {
-			(function(phantomas) {
-				// spy calls to EventTarget.addEventListener
-				// @see https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.addEventListener
-				function eventSpy(eventType) {
-					/* jshint validthis: true */
-					var path = phantomas.getDOMPath(this);
-					phantomas.log('DOM event: "' + eventType + '" bound to "' + path + '"');
-
-					phantomas.incrMetric('eventsBound');
-					phantomas.addOffender('eventsBound', '"%s" bound to "%s"', eventType, path);
-
-					// count window.addEventListener('scroll', ...) - issue #508
-					if (eventType === 'scroll' && (path === 'window' || path === '#document')) {
-						phantomas.incrMetric('eventsScrollBound');
-						phantomas.addOffender('eventsScrollBound', 'bound by %s', phantomas.getBacktrace());
-					}
-				}
-
-				phantomas.spy(Element.prototype, 'addEventListener', eventSpy);
-				phantomas.spy(Document.prototype, 'addEventListener', eventSpy);
-				phantomas.spy(window, 'addEventListener', eventSpy);
-
-				// spy calls to EventTarget.dispatchEvent
-				// @see https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.dispatchEvent
-				phantomas.spy(Element.prototype, 'dispatchEvent', function(ev) {
-					/* jshint validthis: true */
-					var path = phantomas.getDOMPath(this);
-
-					phantomas.log('Core JS event: triggered "%s" on "%s"', ev.type, path);
-
-					phantomas.incrMetric('eventsDispatched');
-					phantomas.addOffender('eventsDispatched', '"%s" on "%s"', ev.type, path);
-				});
-			})(window.__phantomas);
-		});
-	});
+	// inject JS code
+	phantomas.on('init', () => phantomas.injectJs(__dirname + '/scope.js'));
 };
