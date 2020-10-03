c957df5265f4c64ad1d413e2d1d23c04b2f8e6cc
Gaël Métais <gael.metais@gmail.com>
2020-10-04 00:22:06 +0200

    Support for screen resolution option

--- extensions/devices/devices.js
+++ extensions/devices/devices.js
@@ -1,6 +1,5 @@
 /**
  * Provides --phone, --phone-landscape, --tablet and --table-landscape options to force given device viewport and user agent.
- * Also supports the --viewport option to set any device resolution and pixel density ratio.
  */
 'use strict';
 
@@ -15,8 +14,7 @@ module.exports = function(phantomas) {
 			'tablet-landscape': 'Kindle Fire HDX landscape', // 1280x800
 		};
 
-	var device,
-		profileName;
+	var device;
 
 	// check if --phone or --tablet option was passed
 	Object.keys(availableDevices).forEach(function(item) {
@@ -26,40 +24,20 @@ module.exports = function(phantomas) {
 		}
 	});
 
+	// no profile selected, add a hint to the logs
 	if (typeof device === 'undefined') {
-		// no profile selected, add a hint to the logs
 		phantomas.log('No profile selected (available: %s)', Object.keys(availableDevices).join(', '));
-	} else {
-		// apply the profile
-		profileName = availableDevices[device];
-		phantomas.log('Devices: %s provided - using "%s" profile: %j', device, profileName, devices[profileName]);
+		return;
 	}
 
+	// apply the profile
+	const profileName = availableDevices[device];
+
+	phantomas.log('Devices: %s provided - using "%s" profile: %j', device, profileName, devices[profileName]);
+
 	phantomas.on('init', async page => {
-		
-		if (profileName !== undefined) {
-			// @see https://github.com/GoogleChrome/puppeteer/blob/v1.11.0/docs/api.md#pageemulateoptions
-			await page.emulate(devices[profileName]);
-			phantomas.log('page.emulate() called');
-		}
-
-		// if the user sets a viewport size, we assume he/she wants to overwrite the device values
-		const viewport = phantomas.getParam('viewport');
-		if (viewport !== undefined) {
-			phantomas.log('Viewport: %s provided', viewport);
-			
-			// two syntaxes are supported:
-			//  - 1200x800 for 1DPR screens
-			//  - 1200x800x2 for high density screens
-			const viewportValues = viewport.split('x');
-
-			// @see https://github.com/puppeteer/puppeteer/blob/v1.11.0/docs/api.md#pagesetviewportviewport
-			await page.setViewport({
-				width: viewportValues[0],
-				height: viewportValues[1],
-				deviceScaleFactor: viewportValues[2] || 1
-			});
-			phantomas.log('page.setViewport() called');
-		}
+		// @see https://github.com/GoogleChrome/puppeteer/blob/v1.11.0/docs/api.md#pageemulateoptions
+		await page.emulate(devices[profileName]);
+		phantomas.log('page.emulate() called');
 	});
 };
---
+++ extensions/viewport/viewport.js
@@ -0,0 +1,36 @@
+/**
+ * Provides the --viewport option to set any device resolution and pixel density ratio.
+ * If the user sets a viewport size as well as a device option (--phone, --tablet, ...),
+ * we assume that he/she wants to overwrite the device values.
+ *
+ * Two syntaxes are supported:
+ *  - 1200x800 will set a 1x pixel density ratio
+ *  - 1200x800x2 will set the given ratio (float values such as 1.5 are accepted)
+ */
+'use strict';
+
+module.exports = function(phantomas) {
+
+	const viewport = phantomas.getParam('viewport');
+
+	if (viewport === undefined) {
+		phantomas.log('No viewport option specified, will use the device default viewport');
+		return;
+	}
+
+	phantomas.log('Viewport: %s provided', viewport);
+
+	const viewportValues = viewport.split('x');
+	const options = {
+		width: viewportValues[0],
+		height: viewportValues[1],
+		deviceScaleFactor: viewportValues[2] || 1
+	};
+
+	phantomas.on('init', async page => {
+
+		// @see https://github.com/puppeteer/puppeteer/blob/v1.11.0/docs/api.md#pagesetviewportviewport
+		await page.setViewport(options);
+		phantomas.log('page.setViewport() called with options %j', options);
+	});
+};
