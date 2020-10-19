02b2f7da72bc7f89f7e652110d3e38924b9ae9e1
macbre <maciej.brencz@gmail.com>
2020-10-19 21:46:00 +0200

    Migrated fully to commander

--- bin/phantomas.js
+++ bin/phantomas.js
@@ -17,6 +17,7 @@ var url = "";
 
 // parse options
 program
+  .name("phantomas")
   .description(
     "Headless Chromium-based web performance metrics collector and monitoring tool"
   )
@@ -52,107 +53,101 @@ program
   // .string("config")
   // optional params
 
-  //.header("Client options")
+  // Client options
   .option("--phone", "force viewport and user agent of a mobile phone")
   .option("--tablet", "force viewport and user agent of a tablet")
   .option("--viewport <width x height>", "viewport dimensions", "1280x1024")
-  .option("--user-agent <user agent>", "provide a custom user agent");
-/**
-  .header("HTTP options")
-  .option("auth-user", "sets the user name used for HTTP authentication")
-  .option("auth-pass", "sets the password used for HTTP authentication")
+  .option("--user-agent <user agent>", "provide a custom user agent")
+
+  // HTTP options
   .option(
-    "cookie",
+    "--auth-user <user>",
+    "sets the user name used for HTTP authentication"
+  )
+  .option(
+    "--auth-pass <password>",
+    "sets the password used for HTTP authentication"
+  )
+  .option(
+    "--cookie <cookies>",
     'document.cookie formatted string for setting a single cookie (e.g. "bar=foo;domain=url")'
   )
   .option(
-    "cookies-file",
+    "--cookies-file <file>",
     "specifies the file name to store the persistent Cookies"
   )
   .option(
-    "ignore-ssl-errors",
+    "--ignore-ssl-errors",
     "ignores SSL errors, such as expired or self-signed certificate errors"
   )
   .option(
-    "proxy",
+    "--proxy <host:port>",
     "specifies the proxy server to use (e.g. --proxy=192.168.1.42:8080)"
   )
   .option(
-    "proxy-auth",
-    "specifies the authentication information for the proxy (e.g. --proxy-auth=username:password)"
+    "--proxy-auth <username:password>",
+    "specifies the authentication information for the proxy"
   )
   .option(
-    "proxy-type",
+    "--proxy-type <type>",
     "specifies the type of the proxy server [http|socks5|none]"
   )
   .option(
-    "ssl-protocol",
+    "--ssl-protocol <protocol>",
     "sets the SSL protocol for secure connections [sslv3|sslv2|tlsv1|any]"
   )
-  .default("ssl-protocol", "any")
-  .header("Runtime options")
+
+  // Runtime options
   .option(
-    "allow-domain",
+    "--allow-domain <domain>",
     "allow requests to given domain(s) - aka whitelist [domain],[domain],..."
   )
   .option(
-    "block-domain",
+    "--block-domain <domain>",
     "disallow requests to given domain(s) - aka blacklist [domain],[domain],..."
   )
-  .option("disable-js", "disable JavaScript on the page that will be loaded")
-  .boolean("disable-js")
-  .option("no-externals", "block requests to 3rd party domains")
-  .boolean("no-externals")
-  .option("post-load-delay", "wait X seconds before generating a report")
-  .option("scroll", "scroll down the page when it's loaded")
-  .boolean("scroll")
-  .option("spy-eval", "report calls to eval()")
-  .boolean("spy-eval")
-  .option("stop-at-onload", "stop phantomas immediately after onload event")
-  .boolean("stop-at-onload")
-  .option("timeout", "timeout for phantomas run")
-  .default("timeout", 15)
+  .option("--disable-js", "disable JavaScript on the page that will be loaded")
+  .option("--no-externals", "block requests to 3rd party domains")
+  .option("--post-load-delay <N>", "wait X seconds before generating a report")
+  .option("--scroll", "scroll down the page when it's loaded")
+  .option("--spy-eval", "report calls to eval()")
+  .option("--stop-at-onload", "stop phantomas immediately after onload event")
+  .option("--timeout <seconds>", "timeout for phantomas run", 15)
   .option(
-    "wait-for-event",
+    "--wait-for-event <event>",
     "wait for a given phantomas event before generating a report"
   )
   .option(
-    "wait-for-selector",
+    "--wait-for-selector <selector>",
     "wait for an element matching given CSS selector before generating a report"
   )
-  .option("scroll", "scroll down the page when it's loaded")
-  .boolean("scroll")
-  .option("socket", "[experimental] use provided UNIX socket for IPC")
-  .header("Output and reporting")
-  .option("analyze-css", "[experimental] emit in-depth CSS metrics")
-  .boolean("analyze-css")
-  .option("colors", "forces ANSI colors even when output is piped")
-  .boolean("colors")
+  .option("--scroll", "scroll down the page when it's loaded")
+
+  // Output and reporting
+  .option("--analyze-css", "emit in-depth CSS metrics")
+  .option("--colors", "forces ANSI colors even when output is piped")
   .option(
-    "film-strip",
+    "--film-strip",
     "register film strip when page is loading (a comma separated list of milliseconds can be passed)"
   )
-  .boolean("film-strip")
   .option(
-    "film-strip-dir",
+    "--film-strip-dir <dir>",
     "folder path to output film strip (default is ./filmstrip directory)"
   )
-  .option("har", "save HAR to a given file")
-  .option("log", "log to a given file")
-  .option("page-source", "[experimental] save page source to file")
-  .boolean("page-source")
+  .option("--har <file>", "save HAR to a given file")
+  .option("--log <file>", "log to a given file")
+  .option("--page-source", "save page source to file")
   .option(
-    "page-source-dir",
-    "[experimental] folder path to output page source (default is ./html directory)"
+    "--page-source-dir <dir>",
+    "folder path to output page source (default is ./html directory)"
   )
-  .option("reporter", "output format / reporter")
-  .default("reporter", "plain")
-  .alias("reporter", "R")
-  .alias("reporter", "format")
-  .option("screenshot", "render fully loaded page to a given file")
-  .option("silent", "don't write anything to the console")
-  .boolean("silent");
- */
+  .option(
+    "-R, --reporter, --format <reporter>",
+    "output format / reporter",
+    "plain"
+  )
+  .option("--screenshot", "render fully loaded page to a given file")
+  .option("-s, --silent", "don't write anything to the console");
 
 // handle --config (issue #209)
 //program.setConfigFile("config");
@@ -174,11 +169,15 @@ if (typeof options.url === "undefined" && process.argv.length >= 3) {
   }
 }
 
+// handle --no-foo options
+options["no-externals"] = options.externals === false;
+delete options.externals;
+
 // --url is mandatory -> show help
 if (typeof options.url !== "string" && typeof options.config === "undefined") {
   debug("URL not provided - show help and leave");
-  console.log(program.helpInformation());
-  process.exit(255);
+  program.outputHelp();
+  process.exit(1);
 }
 
 url = options.url;
@@ -186,10 +185,6 @@ debug("url: %s", url);
 
 delete options.url;
 delete options.version;
-
-// handle --no-foo options
-options["no-externals"] = options.externals === false;
-delete options.externals;
 
 // add env variable to turn off ANSI colors when needed (#237)
 // suppress this behaviour by passing --colors option (issue #342)
