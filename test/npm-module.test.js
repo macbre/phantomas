e007008bacdc3f7f2d7f9f392a3990db6887eaf8
macbre <maciej.brencz@gmail.com>
2021-05-27 10:15:12 +0100

    jest: refactored results-test.js

--- test/npm-module.test.js
+++ test/npm-module.test.js
@@ -9,7 +9,6 @@ var assert = require("assert"),
 const URL = "http://127.0.0.1:8888/";
 
 describe("CommonJS module's promise", () => {
-
   describe("should resolve a promise", () => {
     it("when provided a valid url", async () => {
       const res = await phantomas(URL);
@@ -17,27 +16,24 @@ describe("CommonJS module's promise", () => {
       assert.strictEqual(res.getUrl(), URL);
       assert.strictEqual(typeof res.getMetrics, "function");
       assert.strictEqual(typeof res.setMetric, "function");
-    })
+    });
   });
 
   describe("should reject a promise", () => {
-
     it("when not provided with URL", async () => {
       try {
         await phantomas(false);
-      }
-      catch (err) {
+      } catch (err) {
         assert.strictEqual(err.message, "URL must be a string");
       }
     });
 
     it("when timed out", async () => {
       try {
-        await phantomas('http://phantomjs.org/', {
-          timeout: 0.001 // [sec]
+        await phantomas("http://phantomjs.org/", {
+          timeout: 0.001, // [sec]
         });
-      }
-      catch (err) {
+      } catch (err) {
         assert.strictEqual(err.message, "Navigation timeout of 1 ms exceeded");
       }
     });
