99604088adda35c92a91b36eb47163045b63e9a8
macbre <maciej.brencz@gmail.com>
2021-07-20 16:29:26 +0100

    ssl-certificate/mkcert.sh: include phantomas.local and 127.0.0.1 domains

--- test/ssl-certificate/mkcert.sh
+++ test/ssl-certificate/mkcert.sh
@@ -5,4 +5,4 @@ echo "Generating SSL certificates for localhost in ${DIR} ... ";
 set -x
 
 mkcert -install
-mkcert -cert-file ${DIR}/localhost.crt -key-file ${DIR}/localhost.key localhost
+mkcert -cert-file ${DIR}/localhost.crt -key-file ${DIR}/localhost.key phantomas.local localhost 127.0.0.1
