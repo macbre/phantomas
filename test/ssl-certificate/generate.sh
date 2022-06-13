0337e6c238b8f76e910834adef70c6d642ac1b7a
macbre <maciej.brencz@gmail.com>
2022-06-13 17:13:00 +0100

    Fix a typo

--- test/ssl-certificate/generate.sh
+++ test/ssl-certificate/generate.sh
@@ -4,5 +4,4 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "Using $SCRIPTPATH directory ..."
 
-mkcert -cert-file ${SCRIPTPATH}/localhost.crt -key-file ${SCRIPTPATH}localhost.key localhost 127.0.0.1 0.0.0.0 ::1
-
+mkcert -cert-file ${SCRIPTPATH}/localhost.crt -key-file ${SCRIPTPATH}/localhost.key localhost 127.0.0.1 0.0.0.0 ::1
