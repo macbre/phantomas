73b4f4f22d14cf1288d6c7e923599351238d1e8f
Maciej Brencz <maciej.brencz@gmail.com>
2022-06-14 10:24:46 +0200

    Use mkcert to set up trusted development certificates (#1168)
    * Update test/ssl-certificate/generate.sh and update the tests README on how to set up mkcert
    
    * CI: install and set up mkcert (#1143)
    
    * Fix a typo
    
    * CI: remove ignore-ssl-errors flags
    
    * mkcert: make it work with puppeteer (see https://github.com/FiloSottile/mkcert/issues/219)
    
    * jest: decrease thresholds as we no longer test ignore-ssl-errors flag
    
    * Container CI: run just a CLI script
    
    Instead of the entire tests suite

--- test/ssl-certificate/generate.sh
+++ test/ssl-certificate/generate.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # https://github.com/FiloSottile/mkcert#installation
-mkcert -cert-file localhost.crt -key-file localhost.key localhost 127.0.0.1 0.0.0.0 ::1
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+echo "Using $SCRIPTPATH directory ..."
+
+mkcert -cert-file ${SCRIPTPATH}/localhost.crt -key-file ${SCRIPTPATH}/localhost.key localhost 127.0.0.1 0.0.0.0 ::1
